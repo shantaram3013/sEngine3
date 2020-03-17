9c53a46eef3a7054f864c58fe9a59393380b0c9f
simsid66 <simsid66@gmail.com>
2020-03-17 21:49:25 +0530

    partially implement css vars, smooth out health bar percentage setter

--- src/UI.js
+++ src/UI.js
@@ -1,4 +1,4 @@
-Game.UI.hideTextBox = function() {
+Game.UI.hideTextBox = function () {
     if (!Game.UI.textBox.hasMouseInside) {
         Game.UI.textBox.style.opacity = 0;
         return;
@@ -6,26 +6,26 @@ Game.UI.hideTextBox = function() {
     Game.UI.closeDialogue();
 }
 
-Game.UI.showTextBox = function() {
+Game.UI.showTextBox = function () {
     Game.UI.textBox.style.opacity = 1;
 }
-Game.UI.setNPCName = function(s) {
+Game.UI.setNPCName = function (s) {
     Game.UI.npcName.innerHTML = s;
 }
 
-Game.UI.setDialogueText = function(s) {
+Game.UI.setDialogueText = function (s) {
     Game.UI.dialogueBox.innerHTML = s;
 }
 
 Game.UI.openDialogue = Game.UI.showTextBox;
 
-Game.UI.dialogueFadeTime = 3000; // milliseconds
+Game.UI.dialogueFadeTime = 3000;
 
-Game.UI.closeDialogue = function() {
+Game.UI.closeDialogue = function () {
     setTimeout(Game.UI.hideTextBox, Game.UI.dialogueFadeTime);
 }
 
-Game.UI.scaleCanvas = function() {
+Game.UI.scaleCanvas = function () {
     Game.canvasScale.x = Game.canvas.width / document.body.clientWidth;
     Game.canvasScale.y = Game.canvas.height / document.body.clientHeight;
 
@@ -33,11 +33,40 @@ Game.UI.scaleCanvas = function() {
     Game.mainCamera.wHeight = (document.body.clientHeight / 2);
 }
 
-Game.UI.setHealthBarPercentage = function(s) {
-    let healthVal = clamp(s, 0, 100);
-    
-    Game.UI.healthBar.style.width = healthVal + '%';
-    Game.UI.healthBar.style.background = `hsl(${healthVal * 1.4}, 89%, 40%)`;
+Game.UI.healthBarDelta = 0.5;
+Game.UI.healthBarIncrementTime = 50;
+Game.UI.healthBarTimeouts = []
 
+Game.UI.setHealthBarPercentage = function (s) {
+
+    let healthVal = clamp(s, 0, 100);
+    if (Game.UI.healthBarTimeouts !== []) {
+        for (let x of Game.UI.healthBarTimeouts) {
+            window.clearTimeout(x);
+        }
+    }
+    if (Game.UI.healthBar.style.width !== "") {
+        widthVal = parseFloat(Game.UI.healthBar.style.width.replace('%', ''));
+    }
+    else {
+        Game.UI.healthBar.style.width = '100%';
+        widthVal = 100;
+    }
+
+    if (widthVal === healthVal) {
+        return 0; 
+    }
+
+    if (widthVal < healthVal) {
+        Game.UI.healthBar.style.width = (widthVal + Game.UI.healthBarDelta) + '%';
+    }
+    else if (widthVal > healthVal) {
+        Game.UI.healthBar.style.width = (widthVal - Game.UI.healthBarDelta) + '%';
+    }
+
+    Game.UI.healthBarTimeouts.push(setTimeout(
+        Game.UI.setHealthBarPercentage, Game.UI.healthBarIncrementTime, healthVal));
+
+    Game.UI.healthBar.style.background = `hsl(${healthVal * 1.4}, 89%, 40%)`;
     Game.UI.healthText.innerHTML = 'fuel cell at ' + healthVal + '%';
 }
